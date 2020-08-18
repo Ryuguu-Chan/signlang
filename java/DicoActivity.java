c1dee96ebdc1e0f72df515b0b1c22970ee262dd9
Ryuguu-Chan <oganozkul@gmail.com>
2020-08-18 03:39:56 +0200

    Create DicoActivity.java

---
+++ java/DicoActivity.java
@@ -0,0 +1,35 @@
+package com.ryuguuchan.outilpourlangagedessignes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DicoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dico);
+    }
+
+    private Signe[][] Elements =
+    {
+            {
+                // A
+                // new Signe("À cause de", )
+                    // TODO
+                    // Filmer les signes afin de les convertir en fichier .gif
+            }
+    };
+
+    public void alphabet_click(View view) {
+        Button actual_button = (Button)view;
+        Intent I = new Intent(this, SigneDicoActivity.class);
+        Bundle B = new Bundle();
+        B.putString("Lettre", actual_button.getText().toString());
+        startActivity(I);
+    }
+}
